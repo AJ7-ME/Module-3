8bb82fc0bd7a1a2aa0df7e5f5cfbc6cd1d1c7882
AJ7-ME <aaravkjoshi2013@gmail.com>
2026-01-27 16:17:13 +1100

    yes

---
+++ HOMEWORK/Sin cos and tan.py
@@ -0,0 +1,9 @@
+import math
+deg = float(input("Enter the angle in degrees: "))
+rad = math.radians(deg)
+sin = math.sin(rad)
+cos = math.cos(rad)
+tan = math.tan(rad)
+print("Sin:", sin)
+print("Cos:", cos)
+print("Tan:", tan)
